2e90124f9cd21c102383188131d77da5546a987b
Smritee <esmritypoudel@gmail.com>
2025-03-23 14:43:21 +0545

    Add files via upload

---
+++ BoxPlots.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5a50f65c-d593-43b9-bb37-e849cf4608fc",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
